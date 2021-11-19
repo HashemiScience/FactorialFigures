4040c59c9315df67c9b5f6b994184d56d4a93fd9
HashemiScience <ali.hashemi@gmail.com>
2021-11-18 19:00:56 -0500

    added new app

--- appOLD.R
+++ appOLD.R
@@ -9,18 +9,16 @@
 
 library(shiny)
 
-
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Figuring out Factorial Figures"),
-   titlePanel("Created by A. Hashemi"),
-   titlePanel("v1.1 - last updated October 5, 2021"),
+   titlePanel("Interpreting Factorial Data"),
    
    # Sidebar with a slider input for number of bins 
    actionButton("runif", "Reset data"),
    hr(),
+   mainPanel(
    plotOutput("barPlot"),
    splitLayout(
      actionButton("runif2", "Show Main Effect of A"),
@@ -29,16 +27,14 @@ ui <- fluidPage(
    splitLayout(
     plotOutput("barPlot2"),
     plotOutput("barPlot3")
-   ),
-   
-   titlePanel("Added features for 1.1: random numbers are now drawn from a uniform distribution from -1 to 1.")
-   
+   )
+   )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
-  my_data <- reactiveValues(data = matrix(sample(runif(4,min=-1,max=1), 4, replace=T),2,2), #c(-1,-0.5,0,0.5,1)
+  my_data <- reactiveValues(data = matrix(sample(c(0.5,1), 4, replace=T),2,2),
                             maineffectA = NULL,
                             maineffectB = NULL
                             #maineffectA = rowMeans(my_data$data),
@@ -47,7 +43,7 @@ server <- function(input, output) {
   #my_data = matrix(sample(c(0.5,1), 4, replace=T),2,2)
   
   observeEvent(input$runif, {
-    my_data$data = matrix(sample(runif(4,min=-1,max=1), 4, replace=T),2,2)
+    my_data$data = matrix(sample(c(0.5,1), 4, replace=T),2,2)
     my_data$maineffectA = NULL
     my_data$maineffectB = NULL
     #my_data$maineffectA = rowMeans(my_data$data)
@@ -73,10 +69,8 @@ server <- function(input, output) {
             legend=c("A1","A2"),
             beside=T,
             args.legend = list(x="bottom"),
-            ylim=c(-1.1,1.1))
+            ylim=c(0,1))
     axis(1, at=colMeans(bp), labels=c("B1","B2"))
-    abline(h=0,lwd=1.5)
-    box(lwd=1.5)
   })
   
   # main effect A
@@ -86,12 +80,10 @@ server <- function(input, output) {
     # Render a barplot
     bp2 <- barplot(my_data$maineffectA,
                    beside=T,
-                   ylim=c(-1,1),
+                   ylim=c(0,1),
                    col=c("black","black"),
                    main="Main Effect of A")
     axis(1, at=bp2, labels=c("A1","A2"))
-    abline(h=0,lwd=1.5)
-    box(lwd=1.5)
   })
 
   # main effect B
@@ -101,12 +93,10 @@ server <- function(input, output) {
     # Render a barplot
     bp3 <- barplot(my_data$maineffectB,
                    beside=T,
-                   ylim=c(-1,1),
+                   ylim=c(0,1),
                    col=c("black","black"),
                    main="Main Effect of B")
     axis(1, at=bp3, labels=c("B1","B2"))
-    abline(h=0,lwd=1.5)
-    box(lwd=1.5)
   })
   
 }
